70fb5e4f7020b4c3220afee55630615320c884f3
Jorge Alonso <jalonsomerchan@gmail.com>
2026-06-15 11:24:23 +0200

    Configure Astro for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jalonsomerchan.github.io",
-  base: "/extretiempo",
+  site: "http://extretiempo.alon.one",
   integrations: [sitemap()]
 });
