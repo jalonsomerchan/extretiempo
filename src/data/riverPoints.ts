0517be4e4487ea38470f60b26b3a59a17808406b
Jorge Alonso <jalonsomerchan@gmail.com>
2026-06-15 12:20:35 +0200

    Add regional river points

---
+++ src/data/riverPoints.ts
@@ -0,0 +1,39 @@
+import type { Location } from "./locations";
+
+export const riverPoints: Location[] = [
+  {
+    slug: "guadiana-badajoz",
+    name: "Guadiana en Badajoz",
+    province: "Badajoz",
+    latitude: 38.8786,
+    longitude: -6.9703,
+  },
+  {
+    slug: "guadiana-merida",
+    name: "Guadiana en Merida",
+    province: "Badajoz",
+    latitude: 38.9161,
+    longitude: -6.3437,
+  },
+  {
+    slug: "tajo-alcantara",
+    name: "Tajo en Alcantara",
+    province: "Caceres",
+    latitude: 39.7182,
+    longitude: -6.8839,
+  },
+  {
+    slug: "jerte-plasencia",
+    name: "Jerte en Plasencia",
+    province: "Caceres",
+    latitude: 40.0312,
+    longitude: -6.0885,
+  },
+  {
+    slug: "alagon-coria",
+    name: "Alagon en Coria",
+    province: "Caceres",
+    latitude: 39.9841,
+    longitude: -6.536,
+  },
+];
